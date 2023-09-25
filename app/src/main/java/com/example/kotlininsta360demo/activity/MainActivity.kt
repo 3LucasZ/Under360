a4240bba33e637efda2fa7ed9db3f32deefd111c
3LucasZ <lucas.zheng@warriorlife.net>
2023-09-24 20:01:38 -0700

    huber360 API implementation part 1

--- app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
@@ -15,32 +15,47 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.jetty.Jetty
 import io.ktor.server.netty.Netty
 
 
 class MainActivity : BaseObserveCameraActivity() {
+    private var mBtnConnect: Button? = null
+    private var mBtnDisconnect: Button? = null
+    private var mBtnCapture: Button? = null
+    private var mBtnLiveActivity: Button? = null
+
+    private var liveIntent: Intent? = null
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         InstaCameraSDK.init(this.application)
         InstaMediaSDK.init(this.application)
 
-        findViewById<Button>(R.id.btn_connect).setOnClickListener{
+        mBtnConnect = findViewById<Button>(R.id.btn_connect);
+        mBtnConnect?.setOnClickListener{
             InstaCameraManager.getInstance().openCamera(InstaCameraManager.CONNECT_TYPE_USB)
         }
-        findViewById<Button>(R.id.btn_disconnect).setOnClickListener{
+        mBtnDisconnect = findViewById<Button>(R.id.btn_disconnect);
+        mBtnDisconnect?.setOnClickListener{
             InstaCameraManager.getInstance().closeCamera()
         }
-        findViewById<Button>(R.id.btn_capture).setOnClickListener{
+        mBtnCapture = findViewById<Button>(R.id.btn_capture);
+        mBtnCapture?.setOnClickListener{
             InstaCameraManager.getInstance().startNormalCapture(false)
         }
-        findViewById<Button>(R.id.btn_live_activity).setOnClickListener {
-            val intent = Intent(this, LiveActivity::class.java)
-            startActivity(intent)
+        liveIntent = Intent(this, LiveActivity::class.java)
+        mBtnLiveActivity = findViewById<Button>(R.id.btn_live_activity);
+        mBtnLiveActivity?.setOnClickListener {
+            startActivity(liveIntent)
         }
 
         val thread: Thread = object : Thread() {
@@ -61,13 +76,42 @@ class MainActivity : BaseObserveCameraActivity() {
         }
         thread.start()
 
-        embeddedServer(Netty, 8080) {
+        embeddedServer(Jetty, 8080) {
             install(ContentNegotiation) {
                 gson {}
             }
             routing {
                 get("/") {
-                    call.respond(mapOf("message" to "Welcome to Insta360!"))
+                    call.respond(mapOf("message" to "Welcome to Huber360 API!"))
+                }
+                get("/command/connect"){
+                    call.respond(mapOf("result" to mBtnConnect?.performClick()));
+                }
+                get("/command/capture"){
+                    if (InstaCameraManager.getInstance().cameraConnectedType!=-1){
+                        InstaCameraManager.getInstance().startNormalCapture(false)
+                        call.respond(mapOf("msg" to "success"));
+                    } else {
+                        call.respond(mapOf("msg" to "err, camera not connected"));
+                    }
+
+                }
+                get("/command/liveActivity"){
+                    if (InstaCameraManager.getInstance().cameraConnectedType!=-1){
+                        startActivity(liveIntent)
+                        call.respond(mapOf("msg" to "success"));
+                    } else {
+                        call.respond(mapOf("msg" to "err, camera not connected"));
+                    }
+                }
+                get("/status/time"){
+                    call.respond(mapOf("time" to System.currentTimeMillis()))
+                }
+                get("/status/connectedType") {
+                    call.respond(mapOf("connectedType" to InstaCameraManager.getInstance().cameraConnectedType))
+                }
+                get("/status/batteryLevel") {
+                    call.respond(mapOf("batteryLevel" to "100"))
                 }
             }
         }.start(wait = false)
