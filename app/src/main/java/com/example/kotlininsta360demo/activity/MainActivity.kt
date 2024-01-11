c045952659e2c19274abdef85749f368b388c4bf
3LucasZ <lucas.zheng@warriorlife.net>
2024-01-11 00:56:47 -0800

    Squashed commit of the following:
    commit 82e70a0adad2237fc9b239cab1650902f5008662
    Author: 3LucasZ <lucas.zheng@warriorlife.net>
    Date:   Thu Jan 11 00:55:47 2024 -0800
    
        code improvements
    
    commit aff0ad15f52c1a55e42d07753b0b011b8e69172d
    Author: 3LucasZ <lucas.zheng@warriorlife.net>
    Date:   Thu Jan 11 00:55:39 2024 -0800
    
        update UI/UX
    
    commit fc03be3bfe72e2610d74dda629da9ae39a047982
    Author: 3LucasZ <lucas.zheng@warriorlife.net>
    Date:   Thu Jan 11 00:55:28 2024 -0800
    
        Remove LiveActivity
    
    commit 249e98dea5ad671b90e3c5dc282ac4efe63e6bdd
    Author: 3LucasZ <lucas.zheng@warriorlife.net>
    Date:   Wed Jan 10 20:57:35 2024 -0800
    
        Working version - update to one pager
    
    commit e8d329191f22930904742acaf02663d1f0ca0e3f
    Author: 3LucasZ <lucas.zheng@warriorlife.net>
    Date:   Sat Dec 23 20:10:54 2023 -0800
    
        Update LiveActivity.kt

--- app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.ToggleButton
 import com.arashivision.sdkcamera.InstaCameraSDK
 import com.arashivision.sdkcamera.camera.InstaCameraManager
 import com.arashivision.sdkcamera.camera.callback.ILiveStatusListener
@@ -30,75 +29,80 @@ import io.ktor.server.jetty.Jetty
 
 
 class MainActivity : BaseObserveCameraActivity(), IPreviewStatusListener, ILiveStatusListener {
+    //UI components to assign to later
+    private var connectBtn: Button? = null
+    private var disconnectBtn: Button? = null
+    private var captureBtn: Button? = null
+    private var startPreviewBtn: Button? = null
+    private var stopPreviewBtn: Button? = null
+    private var startLivestreamBtn: Button? = null
+    private var stopLivestreamBtn: Button? = null
+    private var livestreamStatusText: TextView? = null
+    private var previewView: InstaCapturePlayerView? = null
+
+    //state
+    private var previewResolution: PreviewStreamResolution? = null
+    private var livestreamFPS: Int = 0
+
+    //streaming configuration
     private val rtmp = "rtmp://a.rtmp.youtube.com/live2/g1eg-y0x6-r3e9-mfr3-6twg"
     private val width = 1920;
     private val height = 1080;
     private val fps = 30;
-    private val bitRate =  8 * 1024 * 1024;
+    private val bitRate = 8 * 1024 * 1024;
+    private val panorama = true;
+    private val audioEnabled = true;
+    private val livestreamSettings = LiveParamsBuilder()
+        .setRtmp(rtmp)
+        .setWidth(width)
+        .setHeight(height)
+        .setFps(fps)
+        .setBitrate(bitRate)
+        .setPanorama(panorama)
 
-    private var mBtnConnect: Button? = null
-    private var mBtnDisconnect: Button? = null
-    private var mBtnCapture: Button? = null
-    private var mTvLiveStatus: TextView? = null
-    private var mCapturePlayerView: InstaCapturePlayerView? = null
-    private var mBtnPreview: ToggleButton? = null
-    private var mBtnLive: ToggleButton? = null
-    private var mCurrentResolution: PreviewStreamResolution? = null
-
+    //Initialize (run on app load)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        //base
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //init camera SDK
         InstaCameraSDK.init(this.application)
         InstaMediaSDK.init(this.application)
+        //set UI as variables and assign their behavior
+        connectBtn = findViewById<Button>(R.id.btn_connect);
+        connectBtn?.setOnClickListener {
+            connectCamera()
+        }
+        disconnectBtn = findViewById<Button>(R.id.btn_disconnect);
+        disconnectBtn?.setOnClickListener {
+            disconnectCamera()
+        }
+        captureBtn = findViewById<Button>(R.id.btn_capture);
+        captureBtn?.setOnClickListener {
+            captureImage()
+        }
+        startPreviewBtn = findViewById<Button>(R.id.btn_start_preview);
+        startPreviewBtn?.setOnClickListener {
+            startPreview()
+        }
+        stopPreviewBtn = findViewById<Button>(R.id.btn_stop_preview);
+        stopPreviewBtn?.setOnClickListener {
+            stopPreview()
+        }
+        startLivestreamBtn = findViewById(R.id.btn_start_livestream)
+        startLivestreamBtn?.setOnClickListener {
+            startLivestream()
+        }
+        stopLivestreamBtn = findViewById(R.id.btn_stop_livestream)
+        stopLivestreamBtn?.setOnClickListener {
+            stopLivestream()
+        }
+        previewView = findViewById(R.id.player_capture);
+        previewView!!.setLifecycle(lifecycle);
+        livestreamStatusText = findViewById(R.id.tv_live_status);
 
-        mBtnConnect = findViewById<Button>(R.id.btn_connect);
-        mBtnDisconnect = findViewById<Button>(R.id.btn_disconnect);
-        mBtnCapture = findViewById<Button>(R.id.btn_capture);
-        mBtnPreview = findViewById(R.id.btn_preview)
-        mBtnLive = findViewById(R.id.btn_live)
-        mCapturePlayerView = findViewById(R.id.player_capture);
-        mTvLiveStatus = findViewById(R.id.tv_live_status);
-
-        mBtnConnect?.setOnClickListener{
-            InstaCameraManager.getInstance().openCamera(InstaCameraManager.CONNECT_TYPE_USB)
-        }
-        mBtnDisconnect?.setOnClickListener{
-            InstaCameraManager.getInstance().closeCamera()
-        }
-        mBtnCapture?.setOnClickListener {
-            if (InstaCameraManager.getInstance().cameraConnectedType != -1) InstaCameraManager.getInstance()
-                .startNormalCapture(false)
-        }
-        mBtnPreview?.setOnClickListener {
-            if (mBtnPreview?.isChecked == true){
-                mBtnPreview?.isChecked = InstaCameraManager.getInstance().cameraConnectedType!=-1
-                if (InstaCameraManager.getInstance().cameraConnectedType!=-1){
-                    val list = InstaCameraManager.getInstance()
-                        .getSupportedPreviewStreamResolution(InstaCameraManager.PREVIEW_TYPE_LIVE)
-                    mCurrentResolution = list[0]
-                    InstaCameraManager.getInstance().setPreviewStatusChangedListener(this)
-                    restartPreview();
-                }
-            } else {
-                stopLive();
-                InstaCameraManager.getInstance().closePreviewStream()
-            }
-        }
-        mBtnLive?.setOnClickListener {
-            if (mBtnLive?.isChecked == true){
-                mBtnLive?.isChecked = mBtnPreview?.isChecked == true
-                if (mBtnPreview?.isChecked == true) {
-                    startLive();
-                }
-            } else {
-                stopLive();
-            }
-        }
-        mCapturePlayerView!!.setLifecycle(lifecycle);
-
-
+        //Infinite loop to update top bar UI with latest information
         val thread: Thread = object : Thread() {
             @SuppressLint("SetTextI18n")
             override fun run() {
@@ -108,7 +112,7 @@ class MainActivity : BaseObserveCameraActivity(), IPreviewStatusListener, ILiveS
                         runOnUiThread {
                             // update TextView here!
                             findViewById<TextView>(R.id.tv_cam_status).text =
-                                "T:"+System.currentTimeMillis() +", Connected:"+InstaCameraManager.getInstance().cameraConnectedType+ ", Battery:"+InstaCameraManager.getInstance().cameraCurrentBatteryLevel+", Charging:"+InstaCameraManager.getInstance().isCameraCharging
+                                "T:" + System.currentTimeMillis() + ", Connected:" + InstaCameraManager.getInstance().cameraConnectedType + ", Battery:" + InstaCameraManager.getInstance().cameraCurrentBatteryLevel + ", Charging:" + InstaCameraManager.getInstance().isCameraCharging
                         }
                     }
                 } catch (e: InterruptedException) {
@@ -117,94 +121,120 @@ class MainActivity : BaseObserveCameraActivity(), IPreviewStatusListener, ILiveS
         }
         thread.start()
 
+        //---API ROUTES---
         embeddedServer(Jetty, 8080) {
             install(ContentNegotiation) {
                 gson {}
             }
             routing {
                 get("/") {
-                    call.respond(mapOf("message" to "Welcome to Huber360 API!"))
+                    call.respond(mapOf("msg" to "Welcome to Underwater API!"))
                 }
-                get("/command/connect"){
-                    call.respond(mapOf("result" to mBtnConnect?.performClick()));
+                get("/command/connect") {
+                    connectCamera()
+                    call.respond(mapOf("msg" to "ok"))
                 }
-                get("/command/capture"){
-                    if (InstaCameraManager.getInstance().cameraConnectedType!=-1){
-                        InstaCameraManager.getInstance().startNormalCapture(false)
-                        call.respond(mapOf("msg" to "success"));
+                get("/command/disconnect") {
+                    disconnectCamera()
+                    call.respond(mapOf("msg" to "ok"))
+                }
+                get("/command/capture") {
+                    if (InstaCameraManager.getInstance().cameraConnectedType != -1) {
+                        captureImage()
+                        call.respond(mapOf("msg" to "ok"))
                     } else {
-                        call.respond(mapOf("msg" to "err, camera not connected"));
+                        call.respond(mapOf("err" to "camera not connected"));
                     }
 
                 }
-                get("/command/liveActivity"){
-                    if (InstaCameraManager.getInstance().cameraConnectedType!=-1){
-                        call.respond(mapOf("msg" to "success"));
+                get("/command/startPreview") {
+                    if (InstaCameraManager.getInstance().cameraConnectedType != -1) {
+                        startPreview()
+                        call.respond(mapOf("msg" to "ok"))
                     } else {
-                        call.respond(mapOf("msg" to "err, camera not connected"));
+                        call.respond(mapOf("err" to "camera not connected"));
                     }
                 }
-                get("/status/time"){
+                get("/command/stopPreview") {
+                    if (InstaCameraManager.getInstance().cameraConnectedType != -1) {
+                        stopPreview()
+                        call.respond(mapOf("msg" to "ok"))
+                    } else {
+                        call.respond(mapOf("err" to "camera not connected"));
+                    }
+                }
+                get("/command/startLivestream") {
+                    startLivestream();
+                    call.respond(mapOf("msg" to "ok"))
+                }
+                get("/command/stopLivestream") {
+                    stopLivestream();
+                    call.respond(mapOf("msg" to "ok"))
+                }
+                get("/status/time") {
                     call.respond(mapOf("time" to System.currentTimeMillis()))
                 }
                 get("/status/connectedType") {
                     call.respond(mapOf("connectedType" to InstaCameraManager.getInstance().cameraConnectedType))
                 }
                 get("/status/batteryLevel") {
-                    call.respond(mapOf("batteryLevel" to "100"))
+                    call.respond(mapOf("batteryLevel" to InstaCameraManager.getInstance().cameraCurrentBatteryLevel))
+                }
+                get("/status/fps") {
+                    call.respond(mapOf("fps" to fps))
                 }
             }
         }.start(wait = false)
     }
-    private fun restartPreview() {
-        val builder = PreviewParamsBuilder()
-            .setStreamResolution(mCurrentResolution)
+
+
+
+    //---API FUNCTIONS---
+    private fun connectCamera() {
+        InstaCameraManager.getInstance().openCamera(InstaCameraManager.CONNECT_TYPE_USB)
+    }
+
+    private fun disconnectCamera() {
+        InstaCameraManager.getInstance().closeCamera()
+    }
+
+    private fun captureImage() {
+        InstaCameraManager.getInstance().startNormalCapture(false)
+    }
+
+    private fun startPreview() {
+        val list = InstaCameraManager.getInstance()
+            .getSupportedPreviewStreamResolution(InstaCameraManager.PREVIEW_TYPE_LIVE)
+        if (list.isNotEmpty()) {
+            InstaCameraManager.getInstance().setPreviewStatusChangedListener(this)
+            previewResolution = list[0];
+        }
+        val previewSettings = PreviewParamsBuilder()
+            .setStreamResolution(previewResolution)
             .setPreviewType(InstaCameraManager.PREVIEW_TYPE_LIVE)
-            .setAudioEnabled(false)
+            .setAudioEnabled(audioEnabled)
+        InstaCameraManager.getInstance().startPreviewStream(previewSettings)
+    }
+
+    private fun stopPreview() {
+        stopLivestream()
         InstaCameraManager.getInstance().closePreviewStream()
-        InstaCameraManager.getInstance().startPreviewStream(builder)
+        InstaCameraManager.getInstance().setPreviewStatusChangedListener(null)
+        previewView!!.destroy()
     }
-    private fun startLive() {
-        mCapturePlayerView?.setLiveType(InstaCapturePlayerView.LIVE_TYPE_PANORAMA);
-        // 设置网络ID即可在使用WIFI连接相机时使用4G网络推流
-        val builder = LiveParamsBuilder()
-            .setRtmp(rtmp)
-            .setWidth(width)
-            .setHeight(height)
-            .setFps(fps)
-            .setBitrate(bitRate)
-            .setPanorama(true)
-        InstaCameraManager.getInstance().startLive(builder, this)
+
+    private fun startLivestream() {
+        previewView?.setLiveType(InstaCapturePlayerView.LIVE_TYPE_PANORAMA);
+        InstaCameraManager.getInstance().startLive(livestreamSettings, this)
     }
-    private fun stopLive() {
+
+    private fun stopLivestream() {
         InstaCameraManager.getInstance().stopLive()
     }
-    override fun onOpened() {
-        // Preview stream is on and can be played
-        InstaCameraManager.getInstance().setStreamEncode()
-        mCapturePlayerView!!.setPlayerViewListener(object : PlayerViewListener {
-            override fun onLoadingFinish() {
-                InstaCameraManager.getInstance().setPipeline(mCapturePlayerView!!.pipeline)
-            }
 
-            override fun onReleaseCameraPipeline() {
-                InstaCameraManager.getInstance().setPipeline(null)
-            }
-        })
-        mCapturePlayerView!!.prepare(createParams())
-        mCapturePlayerView!!.play()
-        mCapturePlayerView!!.keepScreenOn = true
-    }
-    override fun onStop() {
-        super.onStop()
-        if (isFinishing) {
-            // Auto close preview after page loses focus
-            InstaCameraManager.getInstance().stopLive()
-            InstaCameraManager.getInstance().closePreviewStream()
-            InstaCameraManager.getInstance().setPreviewStatusChangedListener(null)
-            mCapturePlayerView!!.destroy()
-        }
-    }
+
+
+    //---HELPERS---
     private fun createParams(): CaptureParamsBuilder? {
         return CaptureParamsBuilder()
             .setCameraType(InstaCameraManager.getInstance().cameraType)
@@ -219,35 +249,64 @@ class MainActivity : BaseObserveCameraActivity(), IPreviewStatusListener, ILiveS
             .setLive(true)
             .setGestureEnabled(false) //added
             .setResolutionParams(
-                mCurrentResolution!!.width,
-                mCurrentResolution!!.height,
-                mCurrentResolution!!.fps
+                previewResolution!!.width,
+                previewResolution!!.height,
+                previewResolution!!.fps
             )
     }
 
-    //Status Change
+    //---CALLBACKS---
+    //Activity is no longer the one being viewed callback
+    override fun onStop() {
+        super.onStop()
+        if (isFinishing) {
+            // Auto close preview after page loses focus
+            stopPreview()
+        }
+    }
+    //Preview steam started and playable callback
+    override fun onOpened() {
+        InstaCameraManager.getInstance().setStreamEncode()
+        previewView!!.setPlayerViewListener(object : PlayerViewListener {
+            override fun onLoadingFinish() {
+                InstaCameraManager.getInstance().setPipeline(previewView!!.pipeline)
+            }
+
+            override fun onReleaseCameraPipeline() {
+                InstaCameraManager.getInstance().setPipeline(null)
+            }
+        })
+        previewView!!.prepare(createParams())
+        previewView!!.play()
+        previewView!!.keepScreenOn = true
+    }
+
+    //Camera status changed callback
     override fun onCameraStatusChanged(enabled: Boolean) {
         super.onCameraStatusChanged(enabled)
         if (!enabled) {
-            stopLive();
+            stopLivestream();
         }
     }
 
-    //TV
+    //Live callbacks
     @SuppressLint("SetTextI18n")
     override fun onLivePushError(error: Int, desc: String?) {
-        mTvLiveStatus?.text = "Live Push Error: ($error) ($desc)"
+        livestreamStatusText?.text = "Live Push Error: ($error) ($desc)"
     }
+
     @SuppressLint("SetTextI18n")
     override fun onLiveFpsUpdate(fps: Int) {
-        mTvLiveStatus?.text = "FPS: $fps";
+        livestreamStatusText?.text = "FPS: $fps";
     }
+
     @SuppressLint("SetTextI18n")
     override fun onLivePushStarted() {
-        mTvLiveStatus?.text = "Live Push Started"
+        livestreamStatusText?.text = "Live Push Started"
     }
+
     @SuppressLint("SetTextI18n")
     override fun onLivePushFinished() {
-        mTvLiveStatus?.text = "Live Push Finished"
+        livestreamStatusText?.text = "Live Push Finished"
     }
 }
