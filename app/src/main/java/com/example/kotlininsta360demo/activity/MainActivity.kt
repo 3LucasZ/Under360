27880707bd20d3e09908fdb71836c46822d61c8c
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-17 03:03:38 -0700

    /get/all

--- app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlininsta360demo/activity/MainActivity.kt
@@ -158,6 +158,41 @@ class MainActivity : BaseObserveCameraActivity(), IPreviewStatusListener, ILiveS
                     response["cameraSerial"] = InstaCameraManager.getInstance().cameraSerial
                     call.respond(response)
                 }
+                get("/get/all") {
+                    val response = HashMap<String, Any>()
+                    response["whiteBalance"] = InstaCameraManager.getInstance().getWhiteBalance(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["whiteBalanceValue"] = InstaCameraManager.getInstance().getWhiteBalanceValue(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["ISO"] = InstaCameraManager.getInstance().getISO(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["ISOTopLimit"] = InstaCameraManager.getInstance().getISOTopLimit(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["exposureMode"] = InstaCameraManager.getInstance().getExposureMode(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["exposureEV"] = InstaCameraManager.getInstance().getExposureEV(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["shutterMode"] = InstaCameraManager.getInstance().getShutterMode(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["shutterSpeed"] = InstaCameraManager.getInstance().getShutterSpeed(
+                        FUNCTION_MODE_PREVIEW_STREAM
+                    )
+                    response["captureResolution"] = InstaCameraManager.getInstance().getResolutionFromCamera(
+                        FUNCTION_MODE_CAPTURE_NORMAL)
+                    response["recordResolution"] = InstaCameraManager.getInstance().getResolutionFromCamera(
+                        FUNCTION_MODE_RECORD_NORMAL)
+//                    response["batteryType"] = InstaCameraManager.getInstance().
+//                    response["photoResolution"] = InstaCameraManager.getInstance().getPhotoResolutionFromCamera(
+//                        FUNCTION_MODE_CAPTURE_NORMAL)
+                    call.respond(response)
+                }
                 get("/get/whiteBalance") {
                     call.respond(mapOf("whiteBalance" to InstaCameraManager.getInstance().getWhiteBalance(InstaCameraManager.FUNCTION_MODE_PREVIEW_STREAM)))
                 }
